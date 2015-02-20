9b0d4808252110451cd72849303c0d48c0aa1a88
Austin Drummond <adrummond@reusserdesign.com>
2015-02-20 16:54:46 -0500

    Added factory methods

---
+++ DLAlertView/Classes/DLAVAlertView+FactoryMethods.h
@@ -0,0 +1,40 @@
+//
+//  DLAVAlertView+FactoryMethods.h
+//  DLAlertView
+//
+//  Created by Austin Drummond on 2/20/15.
+//  Copyright (c) 2015 Definite Loop. All rights reserved.
+//
+
+#import "DLAVAlertView.h"
+
+@interface DLAVAlertView (FactoryMethods)
+
++ (instancetype) alertWithTitle:(NSString *)title
+                        message:(NSString*)message
+                       delegate:(id)delegate
+                         cancel:(NSString*)cancel
+                   otherButtons:(NSArray*)otherButtons;
+
++ (instancetype) showAlertWithTitle:(NSString *)title;
+
++ (instancetype) showAlertWithTitle:(NSString *)title
+                            message:(NSString*)message;
+
++ (instancetype) showAlertWithTitle:(NSString *)title
+                            message:(NSString*)message
+                             cancel:(NSString*)cancel;
+
++ (instancetype) showAlertWithTitle:(NSString *)title
+                            message:(NSString*)message
+                             cancel:(NSString*)cancel
+                               done:(NSString*)done
+                     withCompletion:(DLAVAlertViewCompletionHandler)handler;
+
++ (instancetype) showAlertWithTitle:(NSString *)title
+                            message:(NSString*)message
+                             cancel:(NSString*)cancel
+                       otherButtons:(NSArray *)otherButtons
+                     withCompletion:(DLAVAlertViewCompletionHandler)handler;
+
+@end
---
+++ DLAlertView/Classes/DLAVAlertView+FactoryMethods.m
@@ -0,0 +1,101 @@
+//
+//  DLAVAlertView+FactoryMethods.m
+//  DLAlertView
+//
+//  Created by Austin Drummond on 2/20/15.
+//  Copyright (c) 2015 Definite Loop. All rights reserved.
+//
+
+#import "DLAVAlertView+FactoryMethods.h"
+
+@implementation DLAVAlertView (FactoryMethods)
+
++ (instancetype) alertWithTitle:(NSString *)title
+                        message:(NSString*)message
+                       delegate:(id)delegate
+                         cancel:(NSString*)cancel
+                   otherButtons:(NSArray*)otherButtons {
+    
+    DLAVAlertView *av = [[DLAVAlertView alloc] initWithTitle:title
+                                                     message:message
+                                                    delegate:delegate
+                                           cancelButtonTitle:cancel
+                                                buttonTitles:otherButtons];
+    return av;
+}
+
++ (instancetype) alertWithTitle:(NSString *)title
+                        message:(NSString*)message
+                         cancel:(NSString*)cancel
+                           done:(NSString*)done {
+    
+    NSArray *buttons = done ? @[done] : @[];
+    
+    return [self alertWithTitle:title
+                        message:message
+                       delegate:nil
+                         cancel:cancel
+                   otherButtons:buttons];
+}
+
++ (instancetype) showAlertWithTitle:(NSString *)title {
+    
+    return [self showAlertWithTitle:title
+                            message:nil
+                             cancel:nil
+                               done:nil
+                     withCompletion:nil];
+}
+
++ (instancetype) showAlertWithTitle:(NSString *)title
+                            message:(NSString*)message {
+    
+    return [self showAlertWithTitle:title
+                            message:message
+                             cancel:nil
+                               done:nil
+                     withCompletion:nil];
+}
+
++ (instancetype) showAlertWithTitle:(NSString *)title
+                            message:(NSString*)message
+                             cancel:(NSString *)cancel {
+    
+    return [self showAlertWithTitle:title
+                            message:message
+                             cancel:cancel
+                               done:nil
+                     withCompletion:nil];
+}
+
++ (instancetype) showAlertWithTitle:(NSString *)title
+                            message:(NSString*)message
+                             cancel:(NSString*)cancel
+                               done:(NSString*)done
+                     withCompletion:(DLAVAlertViewCompletionHandler)handler {
+    
+    DLAVAlertView *av = [self alertWithTitle:title
+                                     message:message
+                                      cancel:cancel
+                                        done:done];
+    [av showWithCompletion:handler];
+    return av;
+}
+
++ (instancetype) showAlertWithTitle:(NSString *)title
+                            message:(NSString*)message
+                             cancel:(NSString*)cancel
+                       otherButtons:(NSArray *)otherButtons
+                     withCompletion:(DLAVAlertViewCompletionHandler)handler {
+    
+    DLAVAlertView *av = [self alertWithTitle:title
+                                     message:message
+                                    delegate:nil
+                                      cancel:cancel
+                                otherButtons:otherButtons];
+    
+    [av showWithCompletion:handler];
+    return av;
+}
+
+@end
